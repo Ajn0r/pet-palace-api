cf1b65245c7f4c8df7590a74a3cf089aa563fbf9
Ajn0r <ronjasjogren@hotmail.com>
2022-10-20 19:06:15 +0000

    create post views, serializer and urls

---
+++ posts/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for posts
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        """
+        Function to handle image validation
+        """
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'The size of the image cannot be larger than 2MB'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'The width of the image cannot be more than 4069px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'The height of the image cannot be more than 4069px'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'title', 'content', 'image', 'image_filter', 'category',
+            'created_at', 'updated_at',
+        ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
+from pet_palace_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    """
+    Class for viewing posts and creating if logged in.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Post.objects.all().order_by('-created_at')
+
+    def perform_create(self, serializer):
+        """
+        Creates the post and associates it with the signed in user
+        """
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Class for retriving a post to view, edit or delete
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all().order_by('-created_at')
