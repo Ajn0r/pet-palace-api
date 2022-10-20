12e7e5ff4e759b859ed78542e9d97c3ae36031b2
Ajn0r <ronjasjogren@hotmail.com>
2022-10-20 08:59:00 +0000

    add profile detail view and urls

--- profiles/views.py
+++ profiles/views.py
@@ -5,5 +5,17 @@ from pet_palace_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
+    """
+    Class to display all profiles
+    """
     queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Class to edit profiles
+    """
+    queryset = Profile.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
