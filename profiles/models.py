c58ae7905144a4f6bc51c5c95142016624c87588
Ajn0r <ronjasjogren@hotmail.com>
2022-10-20 07:51:32 +0000

    add signals to profiles and create superuser

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
@@ -31,3 +32,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+    def create_profile(sender, instance, created, **kwargs):
+        """
+        Function to create a profile once a user is created.
+        """
+        if created:
+            Profile.objects.create(owner=instance)
+
+    post_save.connect(create_profile, sender=User)
