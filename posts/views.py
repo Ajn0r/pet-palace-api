c7101b58e830f6a8c2b18418722e213787cb1b6c
Ajn0r <ronjasjogren@hotmail.com>
2022-10-21 12:53:01 +0000

    add rest filters to post view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from .models import Post
 from .serializers import PostSerializer
 from pet_palace_api.permissions import IsOwnerOrReadOnly
@@ -14,7 +15,23 @@ class PostList(generics.ListCreateAPIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-    queryset = Post.objects.all().order_by('-created_at')
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter
+    ]
+    ordering_fields = [
+        'comments_count',
+        'likes_count',
+        'likes__created_at'
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
 
     def perform_create(self, serializer):
         """
@@ -31,4 +48,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all().order_by('-created_at')
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
