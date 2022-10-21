83296d77728542ccbbdbb8c2d7cb40f7832b7cec
Ajn0r <ronjasjogren@hotmail.com>
2022-10-21 12:28:28 +0000

    add serializer, view and url to followers

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from pet_palace_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    View class for follower, create and view
+    code is from the Code Institute
+    django rest walkthrough project
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    View class for follower, view and delete
+    code is from the Code Institute
+    django rest walkthrough project
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
