bc74981cc877cc7417d2fa293e9b4c014405c2c3
Ajn0r <ronjasjogren@hotmail.com>
2022-10-22 09:56:56 +0000

    finish pet app tests

--- pets/tests.py
+++ pets/tests.py
@@ -35,3 +35,60 @@ class PetListTest(APITestCase):
         response = self.client.post(
             '/pets/', {'name': 'kitty', 'date_of_birth': '2021-03-02'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_users_pet_count(self):
+        ere = User.objects.get(username='ere')
+        Pet.objects.create(
+            owner=ere, name='kitty', date_of_birth='2020-03-02')
+        Pet.objects.create(
+            owner=ere, name='kitty', date_of_birth='2019-03-02')
+        ere_pets = Pet.objects.filter(owner=ere)
+        self.assertEqual(ere_pets.count(), 3)
+
+
+class PetDetailTest(APITestCase):
+    """
+    Testing the pet detail view
+    The testcode is greatly
+    inspired by the Code Institute
+    django rest framework walkthough.
+    """
+    def setUp(self):
+        ere = User.objects.create_user(username='ere', password='pass')
+        dere = User.objects.create_user(username='dere', password='pass')
+        Pet.objects.create(
+            owner=ere, name='kitty', date_of_birth='2021-03-02')
+        Pet.objects.create(
+            owner=dere, name='doggy', date_of_birth='2020-01-20')
+
+    def test_can_view_pet_with_correct_id(self):
+        response = self.client.get('/pets/1')
+        self.assertEqual(response.data['name'], 'kitty')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_get_pet_with_invalid_id(self):
+        response = self.client.get('/pets/253')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_user_can_update_own_pet(self):
+        self.client.login(username='dere', password='pass')
+        response = self.client.patch('/pets/2', {'name': 'doggi'})
+        pet = Pet.objects.filter(pk=2).first()
+        self.assertEqual(pet.name, 'doggi')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_only_owner_can_update_pet(self):
+        self.client.login(username='ere', password='pass')
+        response = self.client.patch('/pets/2', {'name': 'doggo'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_pet(self):
+        self.client.login(username='dere', password='pass')
+        response = self.client.delete('/pets/2')
+        self.assertEqual(Pet.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_default_type_is_other(self):
+        response = self.client.get('/pets/1')
+        self.assertEqual(response.data['type'], 'other')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
