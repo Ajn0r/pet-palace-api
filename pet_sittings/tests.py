99bc291acb5ce42dc37bce1c21bcdb734c2673ab
Ajn0r <ronjasjogren@hotmail.com>
2022-10-27 07:46:26 +0000

    add tests for petsitting detailed view

--- pet_sittings/tests.py
+++ pet_sittings/tests.py
@@ -54,3 +54,59 @@ class PetSittingTest(APITestCase):
         })
         self.assertNotEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class PetSittingDetailTest(APITestCase):
+    """
+    Class for testing the view
+    for viewing detailed petsittings,
+    updating and deleting petsittings.
+    """
+    def setUp(self):
+        ere = User.objects.create_user(username='ere', password='pass')
+        dere = User.objects.create_user(username='dere', password='pass')
+        petsitting = PetSitting.objects.create(
+            owner=ere, petsitter=dere,
+            compensation='400', location='Stockholm',
+            date_from='2023-01-20', date_to='2023-01-24',
+            description='A pet-sitting'
+        )
+        kitty = Pet.objects.create(
+            owner=ere, date_of_birth='2020-02-20', name='Kitty'
+        )
+        petsitting.pets.set([kitty])
+
+    def test_retrive_petsitting_with_id(self):
+        response = self.client.get('/petsittings/1')
+        self.assertEqual(response.data['compensation'], '400')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrive_petsitting_with_invalid_id(self):
+        response = self.client.get('/petsittings/966')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_owner_can_update_petsitting(self):
+        self.client.login(username='ere', password='pass')
+        response = self.client.patch(
+            '/petsittings/1', {'compensation': '450kr'})
+        petsitting = PetSitting.objects.filter(pk=1).first()
+        self.assertEqual(petsitting.compensation, '450kr')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_others_cannot_update_petsitting(self):
+        self.client.login(username='dere', password='pass')
+        response = self.client.patch(
+            '/petsittings/1', {'compensation': '450kr'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_owner_can_delete_petsitting(self):
+        self.client.login(username='ere', password='pass')
+        response = self.client.delete('/petsittings/1')
+        count = PetSitting.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cannot_delete_other_users_petsitting(self):
+        self.client.login(username='dere', password='pass')
+        response = self.client.delete('/petsittings/1')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
