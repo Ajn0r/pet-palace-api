ba79415fcbe33988533d069884fdbb27abf982d3
Ajn0r <ronjasjogren@hotmail.com>
2022-10-27 06:58:17 +0000

    finish petsittig list view tests

--- pet_sittings/tests.py
+++ pet_sittings/tests.py
@@ -16,7 +16,8 @@ class PetSittingTest(APITestCase):
         petsitting = PetSitting.objects.create(
             owner=ere, petsitter=dere,
             compensation='400', location='Stockholm',
-            date_from='2023-01-20', date_to='2023-01-24'
+            date_from='2023-01-20', date_to='2023-01-24',
+            description='A pet-sitting'
         )
         kitty = Pet.objects.create(
             owner=ere, date_of_birth='2020-02-20', name='Kitty'
@@ -27,3 +28,29 @@ class PetSittingTest(APITestCase):
         response = self.client.get('/petsittings/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_logged_in_user_can_create_petsitting(self):
+        ere = self.client.login(username='ere', password='pass')
+        dere = User.objects.get(username='dere').id
+        kitty = Pet.objects.get(name='Kitty', owner=ere).id
+        response = self.client.post('/petsittings/', {
+            'petsitter': dere, 'compensation': '100kr',
+            'location': 'stockholm', 'date_from': '2023-01-20',
+            'date_to': '2023-01-24', 'pets': kitty,
+            'description': 'A pet-sitting'
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['pets'], [1])
+
+    def test_owner_can_only_choose_from_own_pets(self):
+        ere = self.client.login(username='ere', password='pass')
+        dere = User.objects.get(username='dere')
+        kitty = Pet.objects.create(
+            name='Kitty', owner=dere, date_of_birth='2018-10-14')
+        response = self.client.post('/petsittings/', {
+            'petsitter': dere, 'compensation': '100kr',
+            'location': 'stockholm', 'date_from': '2023-01-20',
+            'date_to': '2023-01-24', 'pets': kitty,
+            'description': 'A pet-sitting'
+        })
+        self.assertNotEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
