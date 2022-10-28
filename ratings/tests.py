76f7aa2b6c32aa91c83937cd6980385db7023cae
Ajn0r <ronjasjogren@hotmail.com>
2022-10-28 07:15:38 +0000

    create tests for rating detailview

--- ratings/tests.py
+++ ratings/tests.py
@@ -64,3 +64,63 @@ class RatingListTest(APITestCase):
             'rate': 4, 'petsitting': petsitting
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class RatingDetailsTest(APITestCase):
+    """
+    Class to test the retrive, update and
+    delete view for the rating model
+    """
+    def setUp(self):
+        ere = User.objects.create_user(username='ere', password='pass')
+        dere = User.objects.create_user(username='dere', password='pass')
+        petsitting = PetSitting.objects.create(
+                owner=ere, petsitter=dere,
+                compensation='400', location='Stockholm',
+                date_from='2023-01-20', date_to='2023-01-24',
+                description='A pet-sitting'
+        )
+        kitty = Pet.objects.create(
+                owner=ere, date_of_birth='2020-02-20', name='Kitty'
+        )
+        petsitting.pets.set([kitty])
+        Rating.objects.create(
+            owner=ere, petsitting=petsitting, rate=5,
+            comment='Very good petsitting'
+        )
+
+    def test_retrive_rating_with_valid_id(self):
+        response = self.client.get('/ratings/1')
+        self.assertEqual(response.data['rate'], 5)
+        self.assertEqual(response.data['comment'], 'Very good petsitting')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_connot_retrive_rating_with_invalid_id(self):
+        response = self.client.get('/ratings/101044')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_owner_can_update_rating(self):
+        self.client.login(username='ere', password='pass')
+        response = self.client.patch(
+            '/ratings/1', {'rate': 4})
+        rating = Rating.objects.filter(pk=1).first()
+        self.assertEqual(rating.rate, 4)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_other_users_can_not_update_rating(self):
+        self.client.login(username='dere', password='pass')
+        response = self.client.patch(
+            '/ratings/1', {'comment': 'The best petsitter ever!!!!'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_owner_can_delete_rating(self):
+        self.client.login(username='ere', password='pass')
+        response = self.client.delete('/ratings/1')
+        self.assertEqual(Rating.objects.count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_other_users_can_not_delete_rating(self):
+        self.client.login(username='dere', password='pass')
+        response = self.client.delete('/ratings/1')
+        self.assertEqual(Rating.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
