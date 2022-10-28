9a29ccfd99713f6886fac8d7faa6e63c9d62693e
Ajn0r <ronjasjogren@hotmail.com>
2022-10-28 09:26:02 +0000

    fix bug on petsitting model on_delete method

--- pet_sittings/models.py
+++ pet_sittings/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 from pets.models import Pet
 
 
+def get_deleted_user():
+    """
+    Sets the petsitter to deleted if user is
+    deleted, so that the petowner's petsittings
+    are not deleted.
+    """
+    deleted_user = User.objects.get(username='Deleted')
+    if deleted_user:
+        return deleted_user.pk
+    raise deleted_user.DoesNotExist('Please add a Deleted User')
+
+
 class PetSitting(models.Model):
     """
     Class for Petsitting model
@@ -13,10 +25,16 @@ class PetSitting(models.Model):
         (0, 'Planned'), (1, 'Ongoing'), (2, 'Finished')
     ]
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='sitting_owner')
+        User, on_delete=models.CASCADE,
+        related_name='sitting_owner'
+        )
     petsitter = models.ForeignKey(
-        User, on_delete=models.SET('deleted'), related_name='petsitter')
-    pets = models.ManyToManyField(Pet, related_name='pets_to_sit', blank=True)
+        User, on_delete=models.SET(get_deleted_user),
+        related_name='petsitter'
+        )
+    pets = models.ManyToManyField(
+        Pet, related_name='pets_to_sit', blank=True
+        )
     description = models.TextField()
     compensation = models.CharField(max_length=130)
     date_from = models.DateField()
