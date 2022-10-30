79edcb359a29f67a02a99e0630d357960bf763aa
Ajn0r <ronjasjogren@hotmail.com>
2022-10-30 13:30:03 +0000

    add code to fix logout issue with dj-rest-auth

--- pet_palace_api/urls.py
+++ pet_palace_api/urls.py
@@ -3,12 +3,13 @@ pet_palace_api URL Configuration
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/',
