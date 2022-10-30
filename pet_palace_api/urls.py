7f73f0900973fe47c92dd73cbe2fe0d1c97929ee
Ajn0r <ronjasjogren@hotmail.com>
2022-10-30 10:43:44 +0000

    add root_route view and url

--- pet_palace_api/urls.py
+++ pet_palace_api/urls.py
@@ -3,8 +3,10 @@ pet_palace_api URL Configuration
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
